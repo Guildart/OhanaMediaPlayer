b5ab9acbf22203df6e08116a71e655ee83668c77
Ladiscou <49318992+Ladiscou@users.noreply.github.com>
2021-05-24 18:20:22 +0200

    ca avance pas tres vite

--- src/Controller/CFilmCreationView.java
+++ src/Controller/CFilmCreationView.java
@@ -4,11 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
 import java.awt.*;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,13 +22,25 @@ public class CFilmCreationView {
     public Button choiceButton;
     public TextField pathText;
     public TextField nameText;
-    public ScrollPane CategoryList;
+    public HBox categoryList;
     public ScrollPane notAllowedUsers;
 
 
 
 
     public void choiceAction(ActionEvent e){
-        System.out.println("ta mere");
+        Stage stage = new Stage();
+        FileChooser myFileChooser = new FileChooser();
+        myFileChooser.setTitle("Sauvegarder un fichier png");
+        myFileChooser.setInitialFileName("Untitled.png");
+        File file = myFileChooser.showOpenDialog(stage);
+        String myPath = file.getPath();
+        this.pathText.setText(myPath);
     }
+
+    public void addCategory(ActionEvent e) {
+        Button testButton = new Button("test");
+        this.categoryList.getChildren().add(testButton);
+    }
+
 }
