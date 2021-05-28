04f91d879f30a2048238a53f3b3fc5a462665b85
pierreGarciaRaf <pierre.garcia@u-psud.fr>
2021-05-28 02:45:54 +0200

    forbidden categories

--- src/Controller/CFilmCreationView.java
+++ src/Controller/CFilmCreationView.java
@@ -1,11 +1,14 @@
 package Controller;
 
+import Model.Account;
+import Model.AccountManagement;
 import Model.CategoriesDB;
 import Model.MoviesDB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -13,7 +16,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
@@ -22,9 +27,7 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class CFilmCreationView implements Initializable{
     @FXML
@@ -33,7 +36,7 @@ public class CFilmCreationView implements Initializable{
     public TextField nameText;
     public SplitMenuButton splitMenuCategory;
     public HBox categoryList;
-    public ScrollPane notAllowedUsers;
+    public HBox notAllowedUsers;
     public Label pathErrorMessage;
     public Label titleErrorMessage;
     public Label errorToAddMessage;
@@ -62,6 +65,7 @@ public class CFilmCreationView implements Initializable{
         cateNSupButton.getChildren().addAll(newCategory, supButton);
         this.categoryList.getChildren().add(cateNSupButton);
         this.categoryAdded.add(categoryName);
+        updateUsersAllowed();
     }
 
     public void supClicked(ActionEvent e){
@@ -117,8 +121,41 @@ public class CFilmCreationView implements Initializable{
         }
     }
 
-    public void updateUserAllowed(){
+    private static boolean hasCommonElements(ArrayList<String> arr1,
+                                           ArrayList<String> arr2)
+    {
 
+        // Check if length of arr1 is greater than 0
+        // and length of arr2 is greater than 0
+        if (arr1.size() > 0 && arr2.size() > 0) {
+            Set<String> firstSet = new HashSet<String>(); //bizarre de créer un hashset mais en fait c'est + rapide
+            for (int i = 0; i < arr1.size(); i++) {
+                firstSet.add(arr1.get(i));
+            }
+
+            // Iterate the elements of the arr2
+            for (int j = 0; j < arr2.size(); j++) {
+                if (firstSet.contains(arr2.get(j))) {//ça fait surement une recherche sur un arbre binaire là
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void updateUsersAllowed(){
+        notAllowedUsers.getChildren().clear();
+        for (Account curAccount : AccountManagement.getAccounts()) {
+            if (hasCommonElements(curAccount.getForbiddenCategories(), categoryAdded)){
+                VBox accountRep = new VBox();
+                ImageView accImgVw = new ImageView(curAccount.getImage());
+                accImgVw.setFitWidth(64);
+                accImgVw.setFitHeight(64);
+                accountRep.setAlignment(Pos.CENTER);
+                accountRep.getChildren().addAll(accImgVw, new Label(curAccount.getUserName() ) );
+                notAllowedUsers.getChildren().add(accountRep);
+            }
+        }
     }
 
 
--- src/Model/Account.java
+++ src/Model/Account.java
@@ -1,31 +1,40 @@
 package Model;
 
 import javafx.scene.image.Image;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class Account {
-    private String userName = "";
-    private String passWord = "";
-    private String imagePath = "";
+    private String userName;
+    private String password;
+    private String imagePath;
     private Role role = Role.other;
+    private ArrayList<String> forbiddenCategories = new ArrayList<String>(0);
 
     Account (JSONObject jsonObject) throws JSONException {
         userName = jsonObject.getString("user name");
-        passWord = jsonObject.getString("password");
+        password = jsonObject.getString("password");
         imagePath = jsonObject.getString("image path");
         role = Role.valueOf(jsonObject.getString("role"));
+        JSONArray forbiddenJSONCat = jsonObject.getJSONArray("forbidden");
+        for (int catNum= 0; catNum < forbiddenJSONCat.length(); catNum++) {
+            forbiddenCategories.add(forbiddenJSONCat.getString(catNum));
+        }
     }
 
-    Account (String userName, String passWord, String imagePath){ // l'admin ne peut ajouter que des comptes other
+    Account (String userName, String password, String imagePath, ArrayList<String> forbiddenCategories){ // l'admin ne peut ajouter que des comptes other
         this.userName = userName;
-        this.passWord = passWord;
+        this.password = password;
         this.imagePath = imagePath;
         this.role = Role.other;
+        this.forbiddenCategories = forbiddenCategories;
     }
 
     public String getPassword() {
-        return passWord;
+        return password;
     }
 
     public String getUserName() {
@@ -43,4 +52,9 @@ public class Account {
     public Role getRole(){
         return role;
     }
+
+    public ArrayList<String> getForbiddenCategories(){
+        return forbiddenCategories;
+    }
+
 }
